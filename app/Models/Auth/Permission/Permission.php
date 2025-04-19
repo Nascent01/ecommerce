72fd92d688d0fa746176f25f3ac4305c50e61d0e
Nascent <nasko6670@abv.bg>
2025-04-19 17:11:33 +0300

    Add fillable properties and relationships to Permission and Role models

--- app/Models/Auth/Permission/Permission.php
+++ app/Models/Auth/Permission/Permission.php
@@ -1,10 +1,19 @@
 <?php
 
-namespace App\Models\Auth;
+namespace App\Models\Auth\Permission;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Auth\Role\Role;
 
 class Permission extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
--- app/Models/Auth/Role/Role.php
+++ app/Models/Auth/Role/Role.php
@@ -1,10 +1,25 @@
 <?php
 
-namespace App\Models\Auth;
+namespace App\Models\Auth\Role;
 
+use App\Models\User\User;
+use App\Models\Auth\Permission\Permission;
 use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    public function permissions()
+    {
+        return $this->belongsToMany(Permission::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
