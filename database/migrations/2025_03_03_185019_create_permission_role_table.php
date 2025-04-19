704952c00c03f825bbc19320b966e36f3722cc89
Nascent <nasko6670@abv.bg>
2025-04-19 15:28:38 +0300

    Add unique name and description fields to permissions and roles tables; update pivot tables for user-role and permission-role relationships

--- database/migrations/2025_03_03_185019_create_permission_role_table.php
+++ database/migrations/2025_03_03_185019_create_permission_role_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Auth\Permission;
+use App\Models\Auth\Role;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,8 +14,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('permission_role', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->foreignIdFor(Permission::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Role::class)->constrained()->cascadeOnDelete();
+
+            $table->primary(['permission_id', 'role_id']);
         });
     }
 
--- database/migrations/2025_03_03_185106_create_role_user_table.php
+++ database/migrations/2025_03_03_185106_create_role_user_table.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\Auth\Role;
+use App\Models\User\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,8 +14,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('role_user', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->foreignIdFor(User::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Role::class)->constrained()->cascadeOnDelete();
+
+            $table->primary(['user_id', 'role_id']);
         });
     }
 
