377d9a3c898a0b526950897863cf7bcdf18c8d49
Nascent <nasko6670@abv.bg>
2025-04-19 13:33:36 +0300

    Add PermissionServiceProvider to manage permissions and roles

---
+++ app/Providers/PermissionServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Providers;
+
+use App\Repositories\Auth\Permission\PermissionRepository;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Contracts\Auth\Access\Gate as GateContract;
+use Illuminate\Support\Facades\Response;
+
+class PermissionServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(GateContract $gate): void
+    {
+        $this->registerPolicies();
+
+        if ($this->app->runningInConsole()) {
+            return;
+        }
+
+        $permissions = (new PermissionRepository)->getPermissionWithRoles();
+
+        foreach ($permissions as $permission) {
+            $gate->define($permission->name, function ($user) use ($permission) {
+                return $user->hasRole($permission->roles)
+                    ? Response::allow()
+                    : Response::deny("Permission '{$permission->name}' needed.");
+            });
+        }
+    }
+}
