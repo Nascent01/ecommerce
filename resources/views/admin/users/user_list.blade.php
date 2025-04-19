ebd1b0be3286ac2af31ff29c305ffbc6765c6eed
Nascent <nasko6670@abv.bg>
2025-04-19 18:20:25 +0300

    Refactor navbar and user list views for improved structure and styling

--- resources/views/admin/users/user_list.blade.php
+++ resources/views/admin/users/user_list.blade.php
@@ -1,35 +1,35 @@
 @extends('admin.layout.layout')
 
 @section('content')
-<div class="container-fluid py-4">
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="table-responsive card">
-            <table class="table table-bordered margin-bottom-0">
-              <thead class="thead-light">
-                <tr>
-                  <th scope="col">ID</th>
-                  <th scope="col">Name</th>
-                  <th scope="col">Email</th>
-                  <th scope="col">Created_at</th>
-                  <th scope="col">Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-              @foreach ($users as $user)
-                <tr>
-                  <th scope="row">{{ $user->id }}</th>
-                  <td>{{ $user->name }}</td>
-                  <td>{{ $user->email }}</td>
-                  <td>{{ $user->created_at }}</td>
-                  <td>
-                  </td>
-                </tr>
-              @endforeach
-              </tbody>
-            </table>
-           </div>
+    <div class="container-fluid py-4">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="table-responsive card">
+                    <table class="table table-bordered margin-bottom-0">
+                        <thead class="thead-light">
+                            <tr>
+                                <th class="text-center" scope="col">ID</th>
+                                <th class="text-center" scope="col">Name</th>
+                                <th class="text-center" scope="col">Email</th>
+                                <th class="text-center" scope="col">Created_at</th>
+                                <th class="text-center" scope="col">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr>
+                                    <th class="text-center" scope="row">{{ $user->id }}</th>
+                                    <td class="text-center">{{ $user->name }}</td>
+                                    <td class="text-center">{{ $user->email }}</td>
+                                    <td class="text-center">{{ $user->created_at }}</td>
+                                    <td class="text-center">
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-       </div>
-  </div>
+    </div>
 @endsection
