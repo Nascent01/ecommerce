eb27031ab81512af40e60e24f0c9a8bd3b001f95
Nascent <nasko6670@abv.bg>
2025-04-19 17:50:17 +0300

    Add AdminAuth middleware to enforce admin role access

---
+++ app/Http/Middleware/Auth/AdminAuth.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware\Auth;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AdminAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = Auth::user();
+
+        if (!$user) {
+            return redirect()->route('login');
+        }
+
+        if (!$user->hasRoleByName('admin')) {
+            abort(403, 'Unauthorized');
+        }
+
+        return $next($request);
+    }
+}
