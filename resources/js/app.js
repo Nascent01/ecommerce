726c1e31f9d8cfb0d7bd6cfb7e82c791febde1f8
Nascent <nasko6670@abv.bg>
2025-04-26 10:54:10 +0300

    Add dynamic module loading for improved script management

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,15 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const modules = import.meta.glob('./*.js');
+for (const path in modules) {
+    if (path !== './app.js') {
+        modules[path]();
+    }
+}
+
+const subModules = import.meta.glob('./**/*.js');
+for (const path in subModules) {
+    subModules[path]();
+}
