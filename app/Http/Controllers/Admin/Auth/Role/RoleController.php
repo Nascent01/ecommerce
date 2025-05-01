4c7a04b0862bfd622fff5c9fffb3b7f51d51909a
Nascent <nasko6670@abv.bg>
2025-05-01 21:21:14 +0300

    Refactor RoleController for improved type hinting and clarity; add role_edit Blade view

--- app/Http/Controllers/Admin/Auth/Role/RoleController.php
+++ app/Http/Controllers/Admin/Auth/Role/RoleController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers\Admin\Auth\Role;
 
 use App\Http\Controllers\Controller;
+use App\Models\Auth\Role\Role;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+use Illuminate\Http\RedirectResponse;
 
 class RoleController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): View
     {
         return view('admin.auth.roles.role_list');
     }
@@ -18,9 +21,9 @@ class RoleController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create(): View
     {
-        //
+        return view('admin.auth.roles.role_edit', ['role' => new Role()]);
     }
 
     /**
@@ -32,34 +35,29 @@ class RoleController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Role $role): View
     {
-        //
+        return view('admin.auth.roles.role_edit', ['role' => $role]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Role $role): RedirectResponse
     {
-        //
+
+        return redirect()->route('admin.roles.edit', $role->id)->with('success', 'Role updated successfully!');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Role $role): RedirectResponse
     {
-        //
+        $role->delete();
+
+        return redirect()->route('admin.roles.index')->with('success', 'Role deleted successfully!');
     }
 }
