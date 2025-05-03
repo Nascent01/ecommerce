77579d4f56bfceb090cffb7c3d2d9fd278e0125b
Nascent <nasko6670@abv.bg>
2025-05-03 10:20:40 +0300

    Add authorization middleware to user and role resource routes in admin panel

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Admin\Auth\Permission\PermissionController;
 use App\Http\Controllers\Admin\Auth\Role\RoleController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\User\UserController;
@@ -10,9 +9,7 @@ Route::name('admin.')->prefix('admin')->middleware('adminAuth')->group(function 
         return view('admin.dashboard.dashboard');
     })->middleware(['auth', 'verified'])->name('dashboard');
 
-    Route::resource('users', UserController::class)->except(['show']);
+    Route::resource('users', UserController::class)->except(['show'])->middleware('can:manage-users');
 
-    Route::resource('roles', RoleController::class)->except(['show']);
-
-    Route::resource('permissions', PermissionController::class)->except(['show']);
+    Route::resource('roles', RoleController::class)->except(['show'])->middleware('can:manage-roles');
 });
