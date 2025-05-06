0e8e17e1a2375f7ea4d58c430d2ca79f46e11da1
Nascent <nasko6670@abv.bg>
2025-05-06 18:11:43 +0300

    Add logo and placeholder image; update logo path in left navigation

--- resources/views/admin/partials/left_navigation.blade.php
+++ resources/views/admin/partials/left_navigation.blade.php
@@ -4,7 +4,7 @@
         <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
             aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0" href="{{ route('admin.dashboard') }}">
-            <img src="{{ asset('themes/custom/css/assets/admin/Logo.min.svg.png') }}" class="navbar-brand-img h-100"
+            <img src="{{ asset('themes/custom/images/Logo.png') }}" class="navbar-brand-img h-100"
                 alt="main_logo">
         </a>
     </div>
