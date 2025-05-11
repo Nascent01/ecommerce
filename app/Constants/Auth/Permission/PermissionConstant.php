c8f0be299d01124d5fc01ada9e57d46ff840a15e
Nascent <nasko6670@abv.bg>
2025-05-11 08:56:17 +0300

    Add permissions for managing products, product categories, and attributes

--- app/Constants/Auth/Permission/PermissionConstant.php
+++ app/Constants/Auth/Permission/PermissionConstant.php
@@ -7,10 +7,16 @@ class PermissionConstant
     const PERMISSION_VIEW_ADMIN_DASHBOARD = 'view-admin-dashboard';
     const PERMISSION_MANAGE_USERS = 'manage-users';
     const PERMISSION_MANAGE_ROLES = 'manage-roles';
+    const PERMISSION_MANAGE_PRODUCTS = 'manage-products';
+    const PERMISSION_MANAGE_PRODUCT_CATEGORIES = 'manage-product-categories';
+    const PERMISSION_MANAGE_ATTRIBUTES = 'manage-attributes';
 
     const PERMISSIONS_ARRAY = [
         self::PERMISSION_VIEW_ADMIN_DASHBOARD => 'View admin dashboard - access to the admin dashboard',
         self::PERMISSION_MANAGE_USERS => 'Manage users - ability to create, update, and delete users',
         self::PERMISSION_MANAGE_ROLES => 'Manage roles - ability to create, update, and delete roles',
+        self::PERMISSION_MANAGE_PRODUCTS => 'Manage products - ability to create, update, and delete products',
+        self::PERMISSION_MANAGE_PRODUCT_CATEGORIES => 'Manage product categories - ability to create, update, and delete product categories',
+        self::PERMISSION_MANAGE_ATTRIBUTES => 'Manage attributes - ability to create, update, and delete product attributes',
     ];
 }
