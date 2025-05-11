245dbc1a6570c48e2bbfd1ed139be53a0bfd0762
Nascent <nasko6670@abv.bg>
2025-05-11 09:04:01 +0300

    Add resource routes for products, product categories, and product attributes with appropriate permissions

--- routes/admin.php
+++ routes/admin.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\Admin\Auth\Role\RoleController;
+use App\Http\Controllers\Admin\Product\ProductCategoryController;
+use App\Http\Controllers\Admin\Product\ProductController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\User\UserController;
+use App\Models\Product\Attribute;
 
 Route::name('admin.')->prefix('admin')->middleware('adminAuth')->group(function () {
     Route::get('/dashboard', function () {
@@ -12,4 +15,8 @@ Route::name('admin.')->prefix('admin')->middleware('adminAuth')->group(function 
     Route::resource('users', UserController::class)->except(['show'])->middleware('can:manage-users');
 
     Route::resource('roles', RoleController::class)->except(['show'])->middleware('can:manage-roles');
+
+    Route::resource('products', ProductController::class)->except(['show'])->middleware('can:manage-products');
+    Route::resource('product-categories', ProductCategoryController::class)->except(['show'])->middleware('can:manage-product-categories');
+    Route::resource('product-attributes', Attribute::class)->except(['show'])->middleware('can:manage-attributes');
 });
