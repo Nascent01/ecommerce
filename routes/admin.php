78f2b0f88340cdf149f0ddcb6d5ad436379f2849
Nascent <nasko6670@abv.bg>
2025-05-31 21:43:47 +0300

    Refactor dashboard route to use Route::view for improved readability

--- routes/admin.php
+++ routes/admin.php
@@ -8,9 +8,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\User\UserController;
 
 Route::name('admin.')->prefix('admin')->middleware('adminAuth')->group(function () {
-    Route::get('/dashboard', function () {
-        return view('admin.dashboard.dashboard');
-    })->middleware(['auth', 'verified'])->name('dashboard');
+    Route::view('/dashboard', 'admin.dashboard.dashboard')->name('dashboard');
 
     Route::resource('users', UserController::class)->except(['show'])->middleware('can:manage-users');
 
