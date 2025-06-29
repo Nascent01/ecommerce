bf49d30249265bb4fe012495d4b8f4162851d386
Nascent <nasko6670@abv.bg>
2025-06-29 16:19:14 +0300

    Refactor layout to use Vite for soft-ui-dashboard CSS and update vite.config.js to include the new stylesheet

--- resources/views/admin/layout/layout.blade.php
+++ resources/views/admin/layout/layout.blade.php
@@ -16,7 +16,8 @@
     <link href="https://demos.creative-tim.com/soft-ui-dashboard/assets/css/nucleo-svg.css" rel="stylesheet" />
     <!-- CSS Files -->
     <link href="{{ asset('themes/custom/css/style.css') }}" rel="stylesheet" />
-    <link id="pagestyle" href="{{ asset('themes/custom/css/soft-ui-dashboard.css') }}" rel="stylesheet" />
+    {{-- <link id="pagestyle" href="{{ asset('themes/custom/css/soft-ui-dashboard.css') }}" rel="stylesheet" /> --}}
+    @vite(['resources/css/soft-ui-dashboard.css'])
 
     @livewireStyles
 
